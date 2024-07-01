01c3b0cedef869725149ac8b81d18c25d5c2510b
Madvith-d <madroux930@gmail.com>
2024-07-01 20:31:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ document.getElementById("generate-btn").onclick=function(){
     let min = document.getElementById("min-value").value;
     console.log(min);
     let max = document.getElementById("max-value").value;
-    let number = Math.floor(Math.random()*(max-min+1)+min);
-    document.getElementById("result-number").textContent = number;
+    if(min>max){
+        document.getElementById("result-number").textContent = "you nigga dont know the difference between minimum amd maximum";
+
+    }
+    else{
+    
+      let number = Math.floor(Math.random()*(max-min+1)+min);
+      document.getElementById("result-number").textContent = number;
+    }
 }
