cdaaf9f668a6d043dd2b68c84262e2cc3687fddd
Madvith-d <madroux930@gmail.com>
2024-07-01 20:55:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById("generate-btn").onclick=function(){
-    let min = document.getElementById("min-value").value;
-    console.log(min);
-    let max = document.getElementById("max-value").value;
+   let min = parseInt(document.getElementById("min-value").value);
+    
+    let max = parseInt(document.getElementById("max-value").value);
     if(min>max){
         document.getElementById("result-number").textContent = "you nigga dont know the difference between minimum amd maximum";
 
